c9316716dbf420d9af5b734aacdcf9913685e59d
Abraham <abrahamputrabs@gmail.com>
2024-10-02 13:48:24 +0700

    Selesai mengerjakan modul 1

---
+++ abrahamputra/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="/">Landing Page</a>
+    <a href="/admin">Admin</a>
+
+    <h1>Admin Page</h1>
+
+</body>
+
+</html>
